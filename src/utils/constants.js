60794aa5ecd040c3317a2ccccc72e09d32314e42
coop0183 <coop0183@gmail.com>
2025-07-31 23:04:30 -0400

    new file:   src/assets/animals/default.jpg 	new file:   src/assets/animals/husky.jpg 	new file:   src/assets/animals/kitten.jpg 	new file:   src/components/Animal.jsx 	modified:   src/components/App.jsx 	modified:   src/components/Header.css 	modified:   src/components/Main.css 	new file:   src/utils/constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const data = [
+  {
+    name: "cat",
+    image: new URL("../assets/animals/kitten.jpg", import.meta.url).href,
+  },
+  {
+    name: "dog",
+    image: new URL("../assets/animals/husky.jpg", import.meta.url).href,
+  },
+  {
+    name: "default",
+    image: new URL("../assets/animals/default.jpg", import.meta.url).href,
+  },
+];
